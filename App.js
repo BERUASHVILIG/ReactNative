c310461c919c7d4b13c2867bd8ddfcf555f28781
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-03-30 20:33:57 +0400

    Styles Inheritance in reactNative, boxShadow property working on android and ios

--- App.js
+++ App.js
@@ -6,14 +6,20 @@ export default function App() {
     <View style={styles.container}>
       <Greet name="giorgi" />
       <Greet name="nika" />
-      <View style={[styles.box, styles.lightblueBg]}>
+      <View style={styles.darkMode}>
+        <Text style={styles.darkModeText}>
+          Style Inheritance <Text style={styles.boldText}>in Bold</Text>
+        </Text>
+      </View>
+      {/* Inheritance from View component in react native isnt working but working in case of Text component */}
+      <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
         <Text style={{ borderRadius: 5, backgroundColor: "red" }}>
           {/* borderradius styleing in Text component only assigned to android version not ios,
            if for ios want borderadius must be wrapped in View comp  */}
           Lightblue Box
         </Text>
       </View>
-      <View style={[styles.box, styles.lightgreenBg]}>
+      <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
         <Text>Lightgreen Box</Text>
       </View>
     </View>
@@ -26,9 +32,18 @@ const styles = StyleSheet.create({
     backgroundColor: "yellow",
     padding: 60,
   },
+  darkMode: {
+    backgroundColor: "black",
+  },
+  darkModeText: {
+    color: "white",
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
   box: {
-    width: 100,
-    height: 100,
+    width: 250,
+    height: 250,
     // padding: 20,
     paddingHorizontal: 10,
     paddingVertical: 20,
@@ -36,11 +51,26 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "green",
     borderStyle: "dashed",
+    borderRadius: 5,
   },
   lightblueBg: {
     backgroundColor: "lightblue",
   },
   lightgreenBg: {
     backgroundColor: "lightgreen",
+  },
+  boxShadow: {
+    shadowColor: "green",
+    shadowOffset: {
+      width: 6,
+      height: 6,
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+    // this boxShadow class properties work on ios version
+  },
+  androidShadow: {
+    elevation: 10,
+    // this wil work on android
   },
 });
