5962b792d6c0afa71b860dbbf69a2cdf9e7348b9
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-03-30 12:57:20 +0400

    Button and Pressable comp, using pressable can create custom functionality for btn

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
-import { Text, View, ScrollView, Image, ImageBackground } from "react-native";
+import { Text, View, Pressable, Image, Button } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "yellow", padding: 60 }}>
-      <ScrollView>
-        <View
-          style={{ backgroundColor: "lightblue", width: 100, height: 100 }}
-        ></View>
-        <View
-          style={{ backgroundColor: "lightgreen", width: 100, height: 100 }}
-        ></View>
+      <Button
+        title="press"
+        onPress={() => console.log("pressed")}
+        color="midnightblue"
+      />
+      <Pressable onPress={() => console.log("Image pressed")}>
         <Image source={logoImg} style={{ width: 100, height: 100 }} />
-        <Image
-          source={{ uri: "https://picsum.photos/100" }}
-          style={{ width: 100, height: 100 }}
-        />
-        <ImageBackground source={logoImg} style={{ width: 100, height: 100 }}>
-          <Text>Text on background</Text>
-        </ImageBackground>
+      </Pressable>
+      <Pressable
+        onPress={() => console.log("Text pressed")}
+        onLongPress={() => console.log("longpressedIn")}
+      >
         <Text>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
@@ -30,7 +27,7 @@ export default function App() {
           more recently with desktop publishing software like Aldus PageMaker
           including versions of Lorem Ipsum.
         </Text>
-      </ScrollView>
+      </Pressable>
     </View>
   );
 }
