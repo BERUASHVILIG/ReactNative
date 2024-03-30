58c3aeb54771dabbc5da4ca4e14d6510f8eca3f9
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-03-30 18:05:24 +0400

    make basic comp, learn small differences in styling between web and mobile

--- App.js
+++ App.js
@@ -1,28 +1,46 @@
-import { View, ActivityIndicator, Button, Alert } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
+import Greet from "./components/Greet";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "yellow", padding: 60 }}>
-      <ActivityIndicator
-        size="large"
-        color="midnightblue"
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        // animating={false} // animating prop for controll visibilty using state
-      />
-      <Button title="Alert" onPress={() => Alert.alert("Invalid data")} />
-      <Button
-        title="Alert2"
-        onPress={() => Alert.alert("Invalid data", "DOB incorrect")}
-      />
-      <Button
-        title="Alert3"
-        onPress={() =>
-          Alert.alert("Invalid data", "DOB incorrect", [
-            { text: "Cencel", onPress: () => console.log("cencel pressed") },
-            { text: "OK", onPress: () => console.log("OK pressed") },
-          ])
-        }
-      />
+    <View style={styles.container}>
+      <Greet name="giorgi" />
+      <Greet name="nika" />
+      <View style={[styles.box, styles.lightblueBg]}>
+        <Text style={{ borderRadius: 5, backgroundColor: "red" }}>
+          {/* borderradius styleing in Text component only assigned to android version not ios,
+           if for ios want borderadius must be wrapped in View comp  */}
+          Lightblue Box
+        </Text>
+      </View>
+      <View style={[styles.box, styles.lightgreenBg]}>
+        <Text>Lightgreen Box</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    padding: 60,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    // padding: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "green",
+    borderStyle: "dashed",
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
+  },
+});
