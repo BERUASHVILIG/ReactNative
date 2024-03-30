8de95f17e6bcb5e57dc9ff5b2b1d0fd037e4f781
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-03-30 22:01:41 +0400

    understand how flex work on View component and other layout styles

--- App.js
+++ App.js
@@ -1,76 +1,26 @@
-import { View, StyleSheet, Text } from "react-native";
-import Greet from "./components/Greet";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Greet name="giorgi" />
-      <Greet name="nika" />
-      <View style={styles.darkMode}>
-        <Text style={styles.darkModeText}>
-          Style Inheritance <Text style={styles.boldText}>in Bold</Text>
-        </Text>
-      </View>
-      {/* Inheritance from View component in react native isnt working but working in case of Text component */}
-      <View style={[styles.box, styles.lightblueBg, styles.boxShadow]}>
-        <Text style={{ borderRadius: 5, backgroundColor: "red" }}>
-          {/* borderradius styleing in Text component only assigned to android version not ios,
-           if for ios want borderadius must be wrapped in View comp  */}
-          Lightblue Box
-        </Text>
-      </View>
-      <View style={[styles.box, styles.lightgreenBg, styles.androidShadow]}>
-        <Text>Lightgreen Box</Text>
-      </View>
+      <Box style={{ backgroundColor: "#342132", flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#265655", flex: 3 }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#257755", flex: 2 }}>Box 3</Box>
+      {/* <Box style={{ backgroundColor: "#755126" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#338598" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#789922" }}>Box 6</Box> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red",
     flex: 1,
-    backgroundColor: "yellow",
-    padding: 60,
-  },
-  darkMode: {
-    backgroundColor: "black",
-  },
-  darkModeText: {
-    color: "white",
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-  box: {
-    width: 250,
-    height: 250,
-    // padding: 20,
-    paddingHorizontal: 10,
-    paddingVertical: 20,
-    marginVertical: 10,
-    borderWidth: 2,
-    borderColor: "green",
-    borderStyle: "dashed",
-    borderRadius: 5,
-  },
-  lightblueBg: {
-    backgroundColor: "lightblue",
-  },
-  lightgreenBg: {
-    backgroundColor: "lightgreen",
-  },
-  boxShadow: {
-    shadowColor: "green",
-    shadowOffset: {
-      width: 6,
-      height: 6,
-    },
-    shadowOpacity: 0.6,
-    shadowRadius: 4,
-    // this boxShadow class properties work on ios version
-  },
-  androidShadow: {
-    elevation: 10,
-    // this wil work on android
+    flexDirection: "row",
+    justifyContent: "center",
   },
 });
