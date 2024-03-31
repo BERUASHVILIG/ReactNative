a856f2febd961e64a8045a8fd6db93b6b5254b6c
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-03-31 13:21:46 +0400

    flexBasis and flex property usecases and how it work

--- App.js
+++ App.js
@@ -4,23 +4,28 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#342132", flex: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#265655", flex: 3 }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#257755", flex: 2 }}>Box 3</Box>
-      {/* <Box style={{ backgroundColor: "#755126" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#342132" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#265655" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#257755", flexBasis: 140, flex: 1 }}>
+        Box 3
+      </Box>
+      <Box style={{ backgroundColor: "#755126", height: 140, flex: 1 }}>
+        Box 4
+      </Box>
+      {/* in this case flexBasis property will increase use of flex property but the box with height property
+       is ignored and take place that is left over   */}
       <Box style={{ backgroundColor: "#338598" }}>Box 5</Box>
-      <Box style={{ backgroundColor: "#789922" }}>Box 6</Box> */}
+      <Box style={{ backgroundColor: "#789922" }}>Box 6</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    height: 300,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
   },
 });
