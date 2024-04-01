7007d0e2737b2dc300d009e2e87146244d014a2b
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2024-04-01 22:30:22 +0400

    add usestate and useEffect for dyanmicly change styles when screen rotates

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
+import { useState, useEffect } from "react";
 import { View, StyleSheet, Text, Dimensions } from "react-native";
 
 export default function App() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  useEffect(() => {
+    const updateDimensions = () => {
+      setDimensions({ window: Dimensions.get("window") });
+    };
+
+    Dimensions.addEventListener("change", updateDimensions);
+
+    return () => {
+      Dimensions.removeEventListener("change", updateDimensions);
+    };
+  }, []);
+
+  const { window } = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome!</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 500 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
       </View>
     </View>
   );
 }
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
 
-console.log({ windowHeight, windowWidth });
+// console.log({ windowHeight, windowWidth });
 
 const styles = StyleSheet.create({
   container: {
@@ -23,13 +52,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 500 ? "60%" : "90%",
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeight > 500 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontSize: windowWidth > 500 ? 50 : 24,
-  },
+  // text: {
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  // },
 });
